b1e9563c6b3915ec1e3220f440f2b2a87ecd31b1
Tako <pierrelouis.hernio@gmail.com>
2020-05-29 19:01:51 +0200

    Started working on the xml parser

--- SuperKaroijuStarFighter/include/squid/engine/tileMapParser.hpp
+++ SuperKaroijuStarFighter/include/squid/engine/tileMapParser.hpp
@@ -1,6 +1,36 @@
 #pragma once
 
+#include <memory>
+#include <string>
+#include <vector>
+#include <m3dia.hpp>
+
+#include "squid/engine/tile.hpp"
+#include "squid/engine/spriteAllocator.hpp"
+#include "squid/engine/object.hpp"
+
 namespace squid
 {
+    using Layer = std::vector<std::shared_ptr<Tile>>;
+    using MapTiles = std::map<std::string, std::shared_ptr<Layer>>;
+    using TileSet = std::unordered_map<unsigned int, std::shared_ptr<TileInfo>>;
 
-}
+    struct TileSheetData
+    {
+        SpriteAllocator &spriteAllocator;
+        m3d::Vector2f tileSize;
+    };
+
+    class TileMapParser
+    {
+    public:
+        TileMapParser(SpriteAllocator &spriteAllocator);
+
+        std::vector<std::shared_ptr<Object>> Parse(const std::string &file, m3d::Vector2f offset);
+
+    private:
+        std::shared_ptr<TileSheetData> BuildTileSheetData();
+
+        SpriteAllocator &m_spriteAllocator;
+    };
+} // namespace squid
